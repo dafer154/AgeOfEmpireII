d13afe86f6a852eba7fa876f385553a2b5c6d2ab
David Zuluaga <david_fza@hotmail.com>
2020-05-02 23:42:23 -0500

    Include modal into the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CivilizationsComponent } from './civilizations/civilizations.component';
 import { UnitsComponent } from './units/units.component';
@@ -9,6 +10,12 @@ import { StructuresComponent } from './structures/structures.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ModalComponent } from './modal/modal.component';
 import { VideoAgeComponent } from './video-age/video-age.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -23,7 +30,18 @@ import { VideoAgeComponent } from './video-age/video-age.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+export interface DialogData {
+  name: string;
+  email: string;
+  phone: string;
+  comment: string;
+}
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +17,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup;
+
+
+  constructor(public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private fb: FormBuilder) { }
+
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern('[a-zA-Z]+([a-zA-Z ]+)*')]],
+      phone: ['', [Validators.required, Validators.pattern('[a-zA-Z]+([a-zA-Z ]+)*')]],
+      comment: ['', [Validators.required, Validators.pattern('[a-zA-Z]+([a-zA-Z ]+)*')]],
+      email: [null, [Validators.required, Validators.email]],
+    });
   }
-
 }
